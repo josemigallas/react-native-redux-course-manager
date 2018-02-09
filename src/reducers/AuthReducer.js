5a66db4ad5e0e9a804ae6350f74f94883a59fc37
Jose Miguel Gallas <josemigallas@gmail.com>
2018-02-09 17:09:13 +0100

    115_creating_immutable_state

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -7,7 +7,7 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-      return;
+      return {...state, email: action.payload };
     default:
       return state
   }
