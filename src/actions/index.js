073aa4c6f25df8416da7c010b720710a68486952
Jose Miguel Gallas <josemigallas@gmail.com>
2018-02-09 18:01:22 +0100

    118_introduction_to_redux_thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
-import { 
+import firebase from "firebase";
+import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED 
+  PASSWORD_CHANGED
 } from "./types";
 
 export const emailChanged = text => {
@@ -15,4 +16,9 @@ export const passwordChanged = text => {
     type: PASSWORD_CHANGED,
     payload: text
   };
+};
+
+export const loginUser = ({ email, password }) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => console.log(user));
 };
