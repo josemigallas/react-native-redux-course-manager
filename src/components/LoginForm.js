4077b58b8e52e56b3bcd1a5bf8c2e5cb6cf29250
Jose Miguel Gallas <josemigallas@gmail.com>
2018-02-09 18:20:43 +0100

    120_redux_thunk_in_practice_continued

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { emailChanged, passwordChanged } from "../actions";
+import { emailChanged, passwordChanged, loginUser } from "../actions";
 import { Card, CardSection, Input, Button } from "./common";
 
 class LoginForm extends Component {
@@ -11,6 +11,12 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -36,13 +42,13 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
       </Card>
     );
-  };
+  }
 }
 
 const mapStateToProps = state => {
@@ -52,4 +58,6 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser
+})(LoginForm);
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -9,10 +9,14 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
+  console.log(action);
+
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
     default:
-      return state
+      return state;
   }
 };
