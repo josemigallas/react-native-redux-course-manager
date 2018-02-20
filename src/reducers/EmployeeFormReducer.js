ca3ec9165cfbc30fecd3e43714e5c4a8448031d4
Jose Miguel Gallas <josemigallas@gmail.com>
2018-02-20 16:35:50 +0100

    134_employee_form_actions

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,14 @@
+import {
+  EMPLOYEE_UPDATE
+} from "../actions/index";
+
+const INITIAL_STATE = {
+
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
