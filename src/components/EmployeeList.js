48fdc7e67f74f693b80fa05c366f89997ffdb71b
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-16 13:41:38 +0100

    149_list_building_in_employee_list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,9 @@
 import map from "lodash.map";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { ListView, View, Text } from "react-native";
+import { ListView } from "react-native";
 import { employeesFetch } from "../actions";
+import ListItem from "./ListItem";
 
 class EmployeeList extends Component {
 
@@ -23,18 +24,18 @@ class EmployeeList extends Component {
     this.dataSource = ds.cloneWithRows(employees);
   }
 
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
+
   render() {
     console.log(this.props);
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
