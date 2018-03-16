9d31c6fa77925104de98857c674e8f1a6276733f
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-16 16:39:10 +0100

    157_texting_employees

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,4 +1,5 @@
 import foreach from "lodash.foreach";
+import Communications from "react-native-communications";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import EmployeeForm from "./EmployeeForm";
@@ -19,13 +20,26 @@ class EmployeeEdit extends Component {
     this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }
 
+  onTextPress() {
+    const { phone, shift } = this.props;
+
+    Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
           </Button>
         </CardSection>
       </Card>
