8f295ed3826cc82ccacf43f0bf4f381e137650f7
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-16 13:34:59 +0100

    148_transforming_objects_to_arrays

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,3 +1,4 @@
+import map from "lodash.map";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { ListView, View, Text } from "react-native";
@@ -23,6 +24,7 @@ class EmployeeList extends Component {
   }
 
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>Employee List</Text>
@@ -37,4 +39,12 @@ class EmployeeList extends Component {
   }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+  const employees = map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
