6b2cee5a557f651da53c13a12199dad79475a504
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-16 11:21:51 +0100

    138_picker_and_style_overrides

--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -3,9 +3,9 @@ import { View } from 'react-native';
 
 const CardSection = (props) => {
   return (
-    <View style={styles.containerStyle}>
+    <View style={[styles.containerStyle, props.style]} >
       {props.children}
-    </View>
+    </View >
   );
 };
 
