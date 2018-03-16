8f11da220a8c08eda8259ce1f01a21e71ce0b64b
Jose Miguel Gallas <josemigallas@gmail.com>
2018-03-16 11:43:40 +0100

    141_creating_records_with_firebase

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import { Picker, Text } from "react-native";
 import { connect } from "react-redux";
-import { employeeUpdate } from "../actions";
+import { employeeUpdate, employeeCreate } from "../actions";
 import { Card, CardSection, Input, Button } from "./common";
 
 class EmployeeCreate extends Component {
+
+  onButtonPressed() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift });
+  }
 
   render() {
     return (
@@ -45,6 +51,7 @@ class EmployeeCreate extends Component {
 
         <CardSection>
           <Button
+            onPress={this.onButtonPressed.bind(this)}
             Create
           />
         </CardSection>
@@ -66,4 +73,7 @@ const mapStateToProps = state => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, {
+  employeeUpdate,
+  employeeCreate
+})(EmployeeCreate);
